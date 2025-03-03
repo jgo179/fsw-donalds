7845e582dd1ca0c8427af5aac5612a04e466d51a
jgo179 <jgo179@gmail.com>
2025-03-03 16:32:19 -0300

    chore: update css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const HomePage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
       <h1 className="text-red-500">Hello World!</h1>
       <Button> ok </Button>
+      <Input />
     </div>
   );
 };
