48accc5136e9dc4bbd1879a48ae338db986b316f
jgo179 <jgo179@gmail.com>
2025-03-21 17:19:20 -0300

    feat: add initial cart context and cart sheet component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Input } from "@/components/ui/input";
 
 const HomePage = () => {
   return (
-    <div className=" border border-red-500 rounded-xl bg-black p-5">
-      <h1 className="text-red-500 p-5">Hello World!</h1>
+    <div className="rounded-xl border border-red-500 bg-black p-5">
+      <h1 className="p-5 text-red-500">Hello World!</h1>
       <Button> ok </Button>
       <Input />
     </div>
--- src/data/get-restaurant-by-slug.ts
+++ src/data/get-restaurant-by-slug.ts
@@ -1,12 +1,11 @@
-import { db } from "@/lib/prisma"
+import { db } from "@/lib/prisma";
 
 export const getRestaurantBySlug = async (slug: string) => {
   const restaurant = await db.restaurant.findUnique({
     where: {
-      slug
-    }
-  })
+      slug,
+    },
+  });
 
-  return     restaurant
-  
-}
+  return restaurant;
+};
