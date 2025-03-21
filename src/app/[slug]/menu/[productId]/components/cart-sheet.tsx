8e5f0578431586208ccdcc98594a92b853abdba0
jgo179 <jgo179@gmail.com>
2025-03-21 17:31:51 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,23 +1,34 @@
 import { useContext } from "react";
 
 import { CartContext } from "@/app/[slug]/contexts/cart";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 
 const CartSheet = () => {
-  const {isOpen, toggleCart} = useContext(CartContext)
-  return ( 
+  const { isOpen, toggleCart, products } = useContext(CartContext);
+  return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-   );
-}
- 
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+        {products.map((product) => (
+          <h1 key={product.id}>
+            {product.name} - {product.quantity}
+          </h1>
+        ))}
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 export default CartSheet;
