1376c9049b30ee9b383088aa1804122b6f227f90
jgo179 <jgo179@gmail.com>
2025-03-21 18:07:07 -0300

    feat: ensure same product can be added more than once to cart

--- src/app/[slug]/contexts/cart.tsx
+++ src/app/[slug]/contexts/cart.tsx
@@ -11,14 +11,14 @@ export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
   toggleCart: () => void;
-  addProduct: (product: CartProduct) => void
+  addProduct: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
   toggleCart: () => {},
-  addProduct: () => {}
+  addProduct: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -29,15 +29,31 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     setIsOpen((prev) => !prev);
   };
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product])
-  }
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProducts) => prevProducts.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
   return (
     <CartContext.Provider
       value={{
         isOpen,
         products,
         toggleCart,
-        addProduct
+        addProduct,
       }}
     >
       {children}
